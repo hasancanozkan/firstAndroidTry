4940233c6fab520fa06f13c6218c8a395c952f05
hasancanozkan <hasan.can.oezkan@fau.de>
2018-01-17 15:04:56 +0100

    bluetooth enabling is added

--- app/src/main/java/com/example/hasancan/firstandroidtry/MainActivity.java
+++ app/src/main/java/com/example/hasancan/firstandroidtry/MainActivity.java
@@ -4,21 +4,50 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.hasancan.firstandroidtry.bluetooth_module.BTManager;
+import com.example.hasancan.firstandroidtry.interfaces.IBluetooth;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.firstandroidtry.MESSAGE";
+
+    Button btn;
+    TextView tV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.button2);
+        tV = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setup(view);
+            }
+        });
     }
-    public void sendMessage(View view){
-        Intent intent = new Intent(this,DisplayMessageActivity.class);
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,message);
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+    public void setup (View view) {
+        IBluetooth myInterface = new BTManager(MainActivity.this);
+        if (!myInterface.checkBluetooth()) {
+            String result = myInterface.setupBluetooth(this);
+            tV.setText(result);
+        }
+        else {
+            myInterface.disableBluetooth();
+            tV.setText("Bluetooth disabled");
+        }
     }
 }
